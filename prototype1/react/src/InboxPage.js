7e44bffd983d2229e4b776d073bc3e342232b3c7
kokkuan <kokkuan01@gmail.com>
2018-08-06 22:28:15 +0800

    Work on Inbox Page

--- prototype1/react/src/InboxPage.js
+++ prototype1/react/src/InboxPage.js
@@ -5,8 +5,16 @@ class InboxPage extends Component{
   render() {
     return (
       <div id="InboxPage">
-        <div className="TopBar"></div>
-        <div></div>
+        <div className="TopBar">
+        </div>
+        <div className="Rest">
+          <div className="Menu">
+            <h1>1323</h1>
+          </div>
+          <div className="Inbox">
+            
+          </div>
+        </div>
       </div>
     );
   }
