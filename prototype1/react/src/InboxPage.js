6489567a3ae9838ab1a7e0b0c7b9038a02f2bc15
kokkuan <kokkuan01@gmail.com>
2018-08-06 23:48:00 +0800

    Work on Inbox Page Interface

--- prototype1/react/src/InboxPage.js
+++ prototype1/react/src/InboxPage.js
@@ -1,18 +1,38 @@
 import React,{Component} from 'react';
 import './InboxPage.css';
+import ReactList from 'react-list';
+import {Link} from 'react-router-dom';
 
 class InboxPage extends Component{
+  renderUnsent(index, key){
+    return(
+      <div
+      key={key}
+      >
+        {index}
+      </div>
+    );
+  }
+
   render() {
     return (
       <div id="InboxPage">
         <div className="TopBar">
+          <Link className="Logout" to="/">Logout</Link>
         </div>
         <div className="Rest">
           <div className="Menu">
-            <h1>1323</h1>
+            <img className="Logo" src="images/logo.png" alt="LOGO"/>
+            <button className="SidebarButton">Unsent</button>
+            <button className="SidebarButton">Replying</button>
+            <button className="SidebarButton">Sent</button>
           </div>
           <div className="Inbox">
-            
+            <ReactList
+              itemRenderer={this.renderUnsent}
+              length={1000}
+              type='variable'
+              axie='x'/>
           </div>
         </div>
       </div>
