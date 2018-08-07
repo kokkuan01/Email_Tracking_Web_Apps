08019fa6c94093dca9c2dce8d3379901e83a47a8
kokkuan <kokkuan01@gmail.com>
2018-08-07 09:51:24 +0800

    Add Sample of Email and Render

--- prototype1/react/src/InboxPage.js
+++ prototype1/react/src/InboxPage.js
@@ -3,13 +3,43 @@ import './InboxPage.css';
 import ReactList from 'react-list';
 import {Link} from 'react-router-dom';
 
+const unsend = [
+  {
+    sender:"Chan Kok Kuan",
+    title:"A package from order 05236788056761 has been shipped",
+    date:"Aug 7"
+  },
+  {
+    sender:"Chan Kok Kuan",
+    title:"Testing",
+    date:"Aug 7"
+  },
+  {
+    sender:"Chan Kok Kuan",
+    title:"Testing",
+    date:"Aug 7"
+  },
+  {
+    sender:"Chan Kok Kuan",
+    title:"Testing",
+    date:"Aug 7"
+  },
+  {
+    sender:"Chan Kok Kuan",
+    title:"Testing",
+    date:"Aug 7"
+  }
+];
+
 class InboxPage extends Component{
   renderUnsent(index, key){
     return(
-      <div
-      key={key}
-      >
-        {index}
+      <div className="EmailDiv" key={key}>
+        <button className="EmailButton">
+          <div className="EmailSender">{unsend[index].sender}</div>
+          <div className="EmailTitle">{unsend[index].title}</div>
+          <div className="EmailDate">{unsend[index].date}</div>
+        </button>
       </div>
     );
   }
@@ -30,7 +60,7 @@ class InboxPage extends Component{
           <div className="Inbox">
             <ReactList
               itemRenderer={this.renderUnsent}
-              length={1000}
+              length={5}
               type='variable'
               axie='x'/>
           </div>
